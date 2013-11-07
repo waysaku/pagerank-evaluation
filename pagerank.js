fea52df562562354dee43a8119d219d3d5c549e8
waysaku <waysaku@gmail.com>
2013-11-07 21:53:54 +0900

    first commit

---
+++ pagerank.js
@@ -0,0 +1,426 @@
+var data_2012_oct = {
+    "tanaka_taro":[
+			{"dest":"kataoka_michio", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3},
+		    ],
+    "kataoka_michio":[
+			{"dest":"tanaka_taro", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"tanaka_taro", "weight":3}
+		    ],
+    "nakai_miho":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "morishita_shinji":[
+			{"dest":"kataoka_michio", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"fujimoto_kiyoshi", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"nakai_miho", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"kataoka_michio", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"tanaka_taro", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ] 
+}
+
+var data_2012_nov = {
+    "tanaka_taro":[
+			{"dest":"takemoto_michiko", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"fujimoto_kiyoshi", "weight":3}
+		    ],
+    "kataoka_michio":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"tanaka_taro", "weight":3}
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"fujimoto_kiyoshi", "weight":3}
+		    ],
+    "nakai_miho":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "morishita_shinji":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"takemoto_michiko", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"tanaka_taro", "weight":3}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "yoshida_masami":[
+			{"dest":"takemoto_michiko", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ] 
+}
+
+var data_2013_jan = {
+    "tanaka_taro":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ],
+    "kataoka_michio":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"sugiyama_takashi", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"sugiyama_takashi", "weight":8},
+			{"dest":"kataoka_michio", "weight":5},
+			{"dest":"takahashi_takamasa", "weight":3}
+		    ],
+    "sugiyama_takashi":[],
+    "nakai_miho":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"kataoka_michio", "weight":5},
+			{"dest":"akiyama_eiji", "weight":3}
+		    ],
+    "morishita_shinji":[
+			{"dest":"kataoka_michio", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"kataoka_michio", "weight":5},
+			{"dest":"takahashi_takamasa", "weight":3}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"sugiyama_takashi", "weight":8},
+			{"dest":"akiyama_eiji", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"kataoka_michio", "weight":5}
+		    ],
+    "yoshida_masami":[
+			{"dest":"takemoto_michiko", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ] 
+}
+
+var data_2013_feb = {
+    "tanaka_taro":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"yoshida_masami", "weight":3}
+		    ],
+    "kataoka_michio":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"fujimoto_kiyoshi", "weight":3}
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "sugiyama_takashi":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "morishita_shinji":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"fujimoto_kiyoshi", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"sugiyama_takashi", "weight":8},
+			{"dest":"akiyama_eiji", "weight":5},
+			{"dest":"yoshida_masami", "weight":3}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"morishita_shinji", "weight":5},
+			{"dest":"takahashi_takamasa", "weight":3}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "yoshida_masami":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"takemoto_michiko", "weight":5},
+			{"dest":"kataoka_michio", "weight":3}
+		    ] 
+}
+
+var data_2013_mar_no_point = {
+    "tanaka_taro":[
+			{"dest":"hachiuma_yoshiko", "weight":1},
+			{"dest":"fujimoto_kiyoshi", "weight":1},
+		    ],
+    "kataoka_michio":[
+			{"dest":"hachiuma_yoshiko", "weight":1},
+			{"dest":"takahashi_takamasa", "weight":1}
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"tanaka_taro", "weight":1},
+			{"dest":"morishita_shinji", "weight":1},
+			{"dest":"takemoto_michiko", "weight":1}
+		    ],
+    "sugiyama_takashi":[
+			{"dest":"tanaka_taro", "weight":1},
+			{"dest":"fujimoto_kiyoshi", "weight":1},
+			{"dest":"takemoto_michiko", "weight":1}
+		    ],
+    "morishita_shinji":[
+			{"dest":"tanaka_taro", "weight":1},
+			{"dest":"fujimoto_kiyoshi", "weight":1},
+			{"dest":"takahashi_takamasa", "weight":1}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"tanaka_taro", "weight":1},
+			{"dest":"fujimoto_kiyoshi", "weight":1},
+			{"dest":"takemoto_michiko", "weight":1},
+			{"dest":"hachiuma_yoshiko", "weight":1}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"fujimoto_kiyoshi", "weight":1}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"hachiuma_yoshiko", "weight":1},
+			{"dest":"morishita_shinji", "weight":1},
+			{"dest":"takahashi_takamasa", "weight":1}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"tanaka_taro", "weight":1},
+			{"dest":"hachiuma_yoshiko", "weight":1},
+			{"dest":"morishita_shinji", "weight":1},
+			{"dest":"takahashi_takamasa", "weight":1},
+			{"dest":"fujimoto_kiyoshi", "weight":1}
+		    ],
+    "yoshida_masami":[
+			{"dest":"fujimoto_kiyoshi", "weight":1},
+			{"dest":"morishita_shinji", "weight":1}
+		    ] 
+}
+
+var data_2013_mar = {
+    "tanaka_taro":[
+			{"dest":"fujimoto_kiyoshi", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"takahashi_takamasa", "weight":3}
+		    ],
+    "kataoka_michio":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+
+		    ],
+    "hachiuma_yoshiko":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"takemoto_michiko", "weight":3}
+		    ],
+    "sugiyama_takashi":[],
+    "morishita_shinji":[
+			{"dest":"fujimoto_kiyoshi", "weight":8},
+			{"dest":"tanaka_taro", "weight":5},
+			{"dest":"takahashi_takamasa", "weight":3}
+		    ],
+    "akiyama_eiji":[
+			{"dest":"tanaka_taro", "weight":8},
+			{"dest":"fujimoto_kiyoshi", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ],
+    "takahashi_takamasa":[
+			{"dest":"fujimoto_kiyoshi", "weight":8},
+			{"dest":"sugiyama_takashi", "weight":5},
+			{"dest":"akiyama_eiji", "weight":3}
+		    ],
+    "fujimoto_kiyoshi":[
+			{"dest":"takahashi_takamasa", "weight":8},
+			{"dest":"hachiuma_yoshiko", "weight":5},
+			{"dest":"morishita_shinji", "weight":3}
+		    ],
+    "takemoto_michiko":[
+			{"dest":"hachiuma_yoshiko", "weight":8},
+			{"dest":"takahashi_takamasa", "weight":5},
+			{"dest":"fujimoto_kiyoshi", "weight":3}
+	],
+    "yoshida_masami":[
+			{"dest":"morishita_shinji", "weight":8},
+			{"dest":"fujimoto_kiyoshi", "weight":5},
+			{"dest":"hachiuma_yoshiko", "weight":3}
+		    ] 
+}
+
+
+
+var data = data_2012_oct;
+
+
+//行列作成用の並び順を定義
+var sort_ctl = {}
+var sort_ctl_byName = new Array();
+var cnt = 0;
+for (var d in data) {
+    sort_ctl[d] = cnt;
+    sort_ctl_byName[cnt] = d;
+    cnt++;
+}
+
+//行列構築
+var dim1_matrix = new Array();
+for (var dim1 in data) {
+    var dim2_matrix = new Array();
+    for(var i = 0; i < cnt; i++) {
+	dim2_matrix[i] = 0;
+    }
+
+    var amount = (function() {
+	var amount_cnt = 0;
+	for (var i = 0; i < data[dim1].length; i++) {
+	    amount_cnt += data[dim1][i].weight;
+	}
+	return amount_cnt;
+    }());
+
+    for (var i = 0; i < data[dim1].length; i++) {
+	var edge = data[dim1][i];
+        dim2_matrix[sort_ctl[edge.dest]] = edge.weight / amount;
+    }
+    dim1_matrix[sort_ctl[dim1]] = dim2_matrix;
+}
+
+//debug
+//var foo = new Matrix(dim1_matrix);
+//alert(foo.Print());
+
+
+
+
+
+var PageRank = function(matrix, loop) {
+    var d = 0.85;
+
+    //行列の転置
+    var transposed_matrix = (function() {
+	var transposed = new Array();
+	for(var i = 0; i < matrix.length; i++) {
+	    transposed[i] = new Array();
+	}
+
+	for (var i = 0; i < matrix.length; i++) {
+	    for (var j = 0; j < matrix[i].length; j++) {
+		transposed[j][i] = matrix[i][j];
+	    }
+	}
+	return transposed;
+    }());
+
+
+    //初回の各PageRankは1と置く
+    var pagerank = (function() {
+        var initial = new Array();
+	for (var i = 0; i < transposed_matrix.length; i++) {
+	    initial[i] = 1;
+	}
+	return initial;
+    }());
+
+    var compute_pr = function() {
+	var new_pagerank = new Array();
+	for (var i = 0; i < transposed_matrix.length; i++) {
+	    var cals = transposed_matrix[i];
+
+	    var refs_pr = 0;
+	    for (var j = 0; j < cals.length; j++) {
+		if (cals[j] > 0) {
+		    refs_pr += pagerank[i] * cals[j];
+		}
+	    }
+	    new_pagerank[i] = (1 - d) + (d * refs_pr);
+	}
+	pagerank = new_pagerank;
+    }
+
+    return function() {
+	for (var i = 0; i < loop; i++) {
+	    compute_pr();
+	}
+	return pagerank;
+    }
+}
+
+//PageRank計算
+var pr_result = new PageRank(dim1_matrix, 15)();
+
+
+
+var result = new Array();
+for (var i = 0; i < pr_result.length; i++) {
+    result.push({"name" : sort_ctl_byName[i], "pagerank" : pr_result[i]});
+}
+result.sort(function(a, b) {
+    return (a.pagerank > b.pagerank ? 1 : -1);
+}).reverse();
+
+var log = "";
+for (var i = 0; i < result.length; i++) {
+    log += result[i].name + ":" + result[i].pagerank + "\n";
+}
+
+console.log(log);
+
